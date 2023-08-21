ce8dc85c5a76cd8db684e70ea6210bbb676f3614
Senu14 <101559338+Senu14@users.noreply.github.com>
2023-08-21 18:33:11 +0200

    First

---
+++ frontend/src/Components/Partials/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className='collected-Nav'>
+    <footer>
+      <p>&copy; 2023 Your Website. All rights reserved.</p>
+    </footer>
+    </div>
+  );
+}
+
+export default Footer;
